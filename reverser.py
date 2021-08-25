01312bdad8f1afd6eb0e9b22032a6287e9fd0368
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-08-25 02:03:45 +0200

    add argument control

--- reverser.py
+++ reverser.py
@@ -4,10 +4,14 @@ import sys
 
 if __name__ == "__main__":
 
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print("insert the program you want to generate the reverse shell")
+        sys.exit(-1)
+
     try:
 
         handler = dbhandler.reverseshell_data()
-        handler.execute()
+        handler.execute(sys.argv[1])
         handler.close_connection()
 
     except:
