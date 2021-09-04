7417008f1235675a02fa23412e24a6681fd1de81
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-09-04 17:49:37 +0200

    finish data migration

--- sql/tableshell.sql
+++ sql/tableshell.sql
@@ -5,7 +5,7 @@ create table shell
 	id_program integer,
 	id_shell integer,
 	
-	command varchar(500) not null,
+	command varchar(8500) not null,
 	connection char(3) not null,
     
 	primary key(id_shell),
